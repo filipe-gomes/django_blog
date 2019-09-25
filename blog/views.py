112541086902e169acd6b455166efaee439e13c7
Filipe <filipegomes404@gmail.com>
2019-09-24 21:05:40 -0500

    Updated views to be class based for the PostList and PostDetail views; Added links to post details; Referred to those links in the titles of blog posts in homepage

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
-def home(request):
-    context = {
-        'posts': Post.objects.all()
-    }
-    return render(request, 'blog/home.html', context)
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' 
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
 
 
 def about(request):
