0d823160a6c8a37780f776dba226d2ba2f78f565
Filipe <filipegomes404@gmail.com>
2019-09-27 22:56:06 -0500

    Added pagination to the homepage

--- blog/views.py
+++ blog/views.py
@@ -10,11 +10,19 @@ from django.views.generic import (
 from .models import Post
 
 
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+
 class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html' 
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 5
 
 
 class PostDetailView(DetailView):
--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ import os
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    image = models.ImageField(default='default.jpeg', upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
