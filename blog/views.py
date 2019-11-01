81a57144990c3029de06e6a61a06058f83434cc5
Filipe <filipegomes404@gmail.com>
2019-10-31 22:39:18 -0500

    Started work on adding support for images on blog posts

--- blog/views.py
+++ blog/views.py
@@ -44,7 +44,7 @@ class PostDetailView(DetailView):
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    fields = ['title', 'content']
+    fields = ['title', 'content', 'image']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
