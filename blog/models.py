b302a5e8aa35a2faacaac2c9c8364ad7934c288f
Filipe <filipegomes404@gmail.com>
2019-11-02 14:07:44 -0500

    Update to About page

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ from django.urls import reverse
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    image = models.ImageField(upload_to='posts/%Y/%m/%d/', null=True, blank=True)
+    image = models.ImageField(upload_to='media/', null=True, blank=True)
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     
